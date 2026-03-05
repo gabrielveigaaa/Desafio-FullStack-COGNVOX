829fbdf00874520ffb303136b1e46eb0f4a251b9
José Gabriel <142948811+gabrielveigaaa@users.noreply.github.com>
2026-03-04 22:38:36 -0300

    atualizaçoes do back e removendo arquivos Docker

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+function verificarToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "Token não fornecido." });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.usuario = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Token inválido." });
+  }
+}
+
+module.exports = verificarToken;
